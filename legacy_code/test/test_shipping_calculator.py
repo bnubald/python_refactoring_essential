8c4f73783ce4b3770b1c61511f024b9a0961e65f
Olof Bjarnason <olof.bjarnason@gmail.com>
2026-06-20 12:35:46 +0200

    Add unit test for ShippingCalculator

--- legacy_code/test/test_shipping_calculator.py
+++ legacy_code/test/test_shipping_calculator.py
@@ -1,9 +1,16 @@
 import unittest
 
+from legacy_code.src.ShippingCalculator import ShippingCalculator
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, True)  # add assertion here
+
+    def test_order_1001(self):
+        shipping_calculator = ShippingCalculator()
+        result = shipping_calculator.calculate_shipping(1001)
+        assert result == 2.5
 
 
 if __name__ == '__main__':
