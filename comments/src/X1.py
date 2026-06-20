1c4e2c0e266d760c4c4fdad6cec2bd6b5fdd0ec0
Olof Bjarnason <olof.bjarnason@gmail.com>
2026-06-20 11:21:04 +0200

    It works!

--- comments/src/X1.py
+++ comments/src/X1.py
@@ -2,14 +2,13 @@ class X1:
 
     @staticmethod
     def m(lower_bound, upper_bound):
-        p = 0
+        accumulated_sum = 0
 
         for i in range(lower_bound, upper_bound + 1):
             # Add square of each number in the range
-            p += X1.square(i)
+            accumulated_sum += X1.square(i)
 
-        # Return accumulated sum
-        return p
+        return accumulated_sum
 
     @staticmethod
     def square(value):
