29596ad5844f23295e72151f57d0d5286878d5ed
Misha Tiurin <650819+tiurin@users.noreply.github.com>
2026-06-20 12:57:48 +0200

    It works!

--- legacy_code/src/ShippingCalculator.py
+++ legacy_code/src/ShippingCalculator.py
@@ -56,20 +56,3 @@ class ShippingCalculator:
         except Exception as e:
             print(e)
             return -1.0
-
-    def get_order(self, order_id: int) -> Order:
-        url = f"https://codemanship.co.uk/api/orders.php?orderId={order_id}"
-
-        response = requests.get(url)
-        response.raise_for_status()
-
-        data = response.json()
-
-        order = Order(
-            orderId=data["orderId"],
-            shippingType=data["shippingType"],
-            weightKg=data["weightKg"],
-            distanceKm=data["distanceKm"],
-            fragile=data["fragile"]
-        )
-        return order
