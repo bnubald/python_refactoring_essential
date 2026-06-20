87f0cf7f1a4bd648a42d347dedb2f08674596468
Olof Bjarnason <olof.bjarnason@gmail.com>
2026-06-20 12:36:52 +0200

    Add unit test for order 1002 in ShippingCalculator

--- legacy_code/test/test_shipping_calculator.py
+++ legacy_code/test/test_shipping_calculator.py
@@ -12,6 +12,10 @@ class MyTestCase(unittest.TestCase):
         result = shipping_calculator.calculate_shipping(1001)
         assert result == 2.5
 
+    def test_order_1002(self):
+        shipping_calculator = ShippingCalculator()
+        result = shipping_calculator.calculate_shipping(1002)
+        assert result == 36.8
 
 if __name__ == '__main__':
     unittest.main()
