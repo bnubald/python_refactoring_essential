bf4986912bada31beea1afbd055adf4a67e63f89
Olof Bjarnason <olof.bjarnason@gmail.com>
2026-06-20 12:54:13 +0200

    It works!

--- legacy_code/src/ShippingCalculator.py
+++ legacy_code/src/ShippingCalculator.py
@@ -11,6 +11,25 @@ class Order:
     fragile: bool
 
 
+class OrderService:
+    def get_order(self, order_id: int) -> Order:
+        url = f"https://codemanship.co.uk/api/orders.php?orderId={order_id}"
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        data = response.json()
+
+        order = Order(
+            orderId=data["orderId"],
+            shippingType=data["shippingType"],
+            weightKg=data["weightKg"],
+            distanceKm=data["distanceKm"],
+            fragile=data["fragile"]
+        )
+        return order
+
+
 class ShippingCalculator:
 
     def calculate_shipping(self, order_id: int) -> float:
