c7b5d2031a35bb3a8c821e0bae02c7c9443ac857
Misha Tiurin <650819+tiurin@users.noreply.github.com>
2026-06-20 13:08:36 +0200

    It works!

--- legacy_code/test/test_shipping_calculator.py
+++ legacy_code/test/test_shipping_calculator.py
@@ -1,12 +1,26 @@
 import unittest
 
-from legacy_code.src.ShippingCalculator import ShippingCalculator
+from legacy_code.src.ShippingCalculator import ShippingCalculator, OrderService, Order
+
+
+class MockOrderService(OrderService):
+
+    def get_order(self, order_id: int) -> Order:
+        order = Order(
+            orderId=1001,
+            shippingType="STANDARD",
+            weightKg=5,
+            distanceKm=120,
+            fragile=False,
+        )
+        return order
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_order_1001(self):
-        shipping_calculator = ShippingCalculator()
+        mock_order_service = MockOrderService()
+        shipping_calculator = ShippingCalculator(mock_order_service)
         result = shipping_calculator.calculate_shipping(1001)
         assert result == 2.5
 
